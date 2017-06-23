037b28297d49ab104a7a141558e6f3eddb82843e
Nikolaj slot Petersen <nikolajslotpetersen@Nikolajs-MacBook-Pro.local>
2017-06-23 17:04:14 +0200

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'NSPwebPM',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-93-250-90.eu-central-1.compute.amazonaws.com',
+      key: '~/.ssh/nikolajslot.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:nikolajspetersen/website.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
